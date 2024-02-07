6a42eef5c39fba280079bc3524adfb900dd96aa9
Ari Jaya Teguh <145629681+Spacebone31@users.noreply.github.com>
2024-02-07 16:36:03 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ public class Main {
         Car car1 = new Car(200, "Toyota", 2020);
 
         // Run the methods
-        car1.printDetails(); // The car is Toyota, made in 2020, and has a max speed of 200 km/h.
+        car1.printDetails();
         car1.setMaxSpeed(220); // Change the max speed
-        car1.printDetails(); // The car is Toyota, made in 2020, and has a max speed of 220 km/h.
+        car1.printDetails();
     }
 }
